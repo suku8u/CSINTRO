d32db9525d2315bbd1e4d649c1b08983b5d388a6
Sukumar Ponram <suku8u@uw.edu>
2015-07-02 22:14:03 -0400

    first exercise files

---
+++ CSIntro/CSIntro/Name.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSIntro
+{
+	public class Name
+	{
+		public static void main ()
+		{
+			System.Console.WriteLine ("1");
+			System.Console.WriteLine ("101");
+			System.Console.WriteLine ("1001");
+		}
+	}
+}
+
--- CSIntro/CSIntro/Program.cs
+++ CSIntro/CSIntro/Program.cs
@@ -1,7 +1,23 @@
-﻿class HelloCSharp
+﻿using System;
+
+class HelloCSharp
 {
 	static void Main()
 	{
-		System.Console.WriteLine ("Hello C# !!");
+		/*throw new System.NotImplementedException ("Intended exception.");
+		System.Console.WriteLine ("Hello C# !!");*/
+		CSIntro.Name.main ();
+		Console.WriteLine ("Good Day !");
+		DateTime now = DateTime.Now;
+		Console.WriteLine (now);
+		Double squareroot = Math.Sqrt (12345);
+		Console.WriteLine (squareroot);
+		for (int i = 2; i <= 100; i++) {
+			if (i % 2 != 0) {
+				Console.WriteLine (-i);
+			} else {
+				Console.WriteLine (i);
+			}
+		}
 	}
 }
